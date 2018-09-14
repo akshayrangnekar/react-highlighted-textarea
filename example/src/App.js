b569a839a038885634d25eb28707d4ad98cf7441
Akshay Rangnekar <akshay@ycomplex.com>
2018-09-14 14:23:08 +0100

    Initial working version

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react'
+import HighlightedTextArea from 'react-highlighted-textarea';
 
-import ExampleComponent from 'react-highlighted-textarea'
+function calcHighlights(input) {
+  if (input.length > 10) {
+    return [
+      [0, 5, 'first'], 
+      [7, 10], 
+      [input.length - 10, input.length - 5, 'next']
+    ];
+  }
+  return [
+    [0, 5]
+  ];
+}
+
+const initialText = 'This is the initial text';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: initialText, 
+      highlights: calcHighlights(initialText)
+    }
+  }
+
+  handleChange(text) {
+    const highlights = calcHighlights(text);
+    // console.log(text);
+    this.setState({
+      text, highlights
+    });
+  }
+
+  handleBlur(e) {
+    console.log('Blur event', e);
+  }
+
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <HighlightedTextArea value={this.state.text} highlights={this.state.highlights} onBlur={(e) => this.handleBlur(e)} onChange={(e) => this.handleChange(e)}/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,101 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export default class HighlightedTextarea extends Component {
   static propTypes = {
-    text: PropTypes.string
+    value: PropTypes.string,
+    highlights: PropTypes.array,
+    onChange: PropTypes.func.isRequired,
+    onBlur: PropTypes.func
+  }
+
+  static OPEN_MARK = '<mark>';
+  static CLOSE_MARK = '</mark>';
+
+  constructor(props) {
+    super(props);
+    this._backdropRef = React.createRef();
+    this._handleInputChange = this._handleInputChange.bind(this);
+    this._handleScroll = this._handleScroll.bind(this);
+  }
+
+  _handleInputChange(event) {
+    this.props.onChange(event.target.value);
+  }
+
+  _handleScroll(event) {
+    const scrollTop = event.target.scrollTop;
+    this._backdropRef.current.scrollTop = scrollTop;
+  }
+
+  _handleRegexHighlight(input, payload) {
+    return input.replace(payload, HighlightedTextarea.OPEN_MARK + '$&' + HighlightedTextarea.CLOSE_MARK);
+  }
+
+  _handleArrayHighlight(input, payload) {
+    let offset = 0;
+    payload.forEach(function(element) {
+      // insert open tag
+      var open = element[0] + offset;
+
+      if (element[2]) {
+        const OPEN_MARK_WITH_CLASS = '<mark class="' + element[2] + '">';
+        input = input.slice(0, open) + OPEN_MARK_WITH_CLASS + input.slice(open);
+        offset += OPEN_MARK_WITH_CLASS.length;
+      } else {
+        input = input.slice(0, open) + HighlightedTextarea.OPEN_MARK + input.slice(open);
+        offset += HighlightedTextarea.OPEN_MARK.length;
+      }
+
+      // insert close tag
+      var close = element[1] + offset;
+
+      input = input.slice(0, close) + HighlightedTextarea.CLOSE_MARK + input.slice(close);
+      offset += HighlightedTextarea.CLOSE_MARK.length;
+    }, this);
+    return input;
+  }
+
+  getHighlights() {
+    let content = this.props.value;
+    let highlights = this.props.highlights;
+    let highlightMarks = content;
+
+    // escape HTML
+    content = content.replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+
+    if (content) {
+      highlightMarks = this._handleArrayHighlight(content, highlights);
+    }
+
+    // this keeps scrolling aligned when input ends with a newline
+    highlightMarks = highlightMarks.replace(new RegExp('\\n(' + HighlightedTextarea.CLOSE_MARK + ')?$'), '\n\n$1');
+
+    // highlightMarks = highlightMarks.replace(new RegExp(HighlightedTextarea.OPEN_MARK, 'g'), '<mark>');
+    // highlightMarks = highlightMarks.replace(new RegExp(HighlightedTextarea.CLOSE_MARK, 'g'), '</mark>');
+
+    return highlightMarks;
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={`rhta-container ${styles['rhta-container']}`} >
+        <div className={`rhta-backdrop ${styles['rhta-backdrop']}`} ref={this._backdropRef}>
+          <div
+            className={`rhta-highlights rhta-content ${styles['rhta-highlights']} ${styles['rhta-content']}`}
+            dangerouslySetInnerHTML={{__html: this.getHighlights()}}
+          />
+        </div>
+        <textarea
+          className={`rhta-input rhta-content ${styles['rhta-input']} ${styles['rhta-content']}`}
+          onChange={this._handleInputChange}
+          onScroll={this._handleScroll}
+          onBlur={(e) => this.props.onBlur(e.target.value)}
+          value={this.props.value}
+        />
       </div>
-    )
+    );
   }
 }
